49cd20f60c9db616c18c76df5478f13859f75147
David Ghergut <davidghergut@gmail.com>
2021-02-02 22:08:07 +0200

    Spymania

--- Pi_Game/Game.py
+++ Pi_Game/Game.py
@@ -13,19 +13,23 @@ from time import sleep
 #VARIABILE GLOBALE (GLOBAL VARIABLES):
 sense = SenseHat()
 is_playable = [1, 1, 0, 0, 0, 0]
-levels = {0:"Storyline", 1:"Level 1", 2:"Level 2", 3:"Level 3", 4:"Level 4", 5:"Level5 -> Boss Fight"}
+levels = {0:"Storyline", 1:"Level 1", 2:"Level 2", 3:"Level 3", 4:"Level 4", 5:"Level 5 -> Boss Fight"}
 
 #CULORI pentru SenseHat (COLOURS for the SenseHat):
 black = (0, 0, 0)
 cyan = (0, 255, 255)
 blue = (0, 0, 255)
+darker_blue = (0, 102, 204)
 red = (255, 0, 0)
 gold = (255, 215, 0)
 green = (0, 255, 0)
 white = (255, 255, 255)
 purple = (128, 0, 128)
+orange = (255, 140, 0)
+saddlebrown = (139, 69, 19)
+coral = (255, 160, 122)
 
-#Aceasta functie va afisa imaginea de sfarsit al primului nivel (This function will print the ending image for the first level)
+#Aceasta functie va afisa imaginea de sfarsit a tuturor nivelelor (This function will print the ending image for all the levels)
 imagine_de_sfarsit_bucata_1 = [
     black, black, black, black, black, black, black, black,
     black, black, black, black, black, black, black, black,
@@ -44,14 +48,83 @@ def afisare_imagine_de_sfarsit_de_nivel_1():
         sleep(0.5)
     sense.clear()
 
-#Aceasta functie va afisa a doua imagine de sfarsit al primului nivel (This function will print the second ending image for the first level)
-"""
-imagine_de_sfarsit_bucata_2 = [
-        
+#Aceasta functie va afisa a doua imagine de sfarsit a tuturor nivelelor (This function will print the second ending image for all the levels)
+
+imagine_de_sfarsit_bucata_2_etapa_1 = [
+    black, black, black, black, black, black, black, black,
+    black, blue, blue, black, black, green, green, black,
+    black, black, blue, blue, green, green, black, black,
+    black, black, black, blue, green, black, black, black,
+    black, black, black, red, gold, black, black, black,
+    black, black, red, red, gold, gold, black, black,
+    black, red, red, black, black, gold, gold, black,
+    black, black, black, black, black, black, black, black,
+]
+imagine_de_sfarsit_bucata_2_etapa_2 = [
+    black, black, black, black, black, black, black, black,
+    black, blue, blue, black, black, green, green, black,
+    black, black, blue, blue, green, green, black, black,
+    black, black, black, white, white, black, black, black,
+    black, black, black, white, white, black, black, black,
+    black, black, red, red, gold, gold, black, black,
+    black, red, red, black, black, gold, gold, black,
+    black, black, black, black, black, black, black, black,
+]
+imagine_de_sfarsit_bucata_2_etapa_3 = [
+    black, black, black, black, black, black, black, black,
+    black, blue, blue, black, black, green, green, black,
+    black, black, blue, white, white, green, black, black,
+    black, black, white, white, white, white, black, black,
+    black, black, white, white, white, white, black, black,
+    black, black, red, white, white, gold, black, black,
+    black, red, red, black, black, gold, gold, black,
+    black, black, black, black, black, black, black, black,
+]
+imagine_de_sfarsit_bucata_2_etapa_4 = [
+    blue, blue, black, black, black, black, green, green,
+    black, blue, blue, white, white, green, green, black,
+    black, white, white, white, white, white, white, black,
+    black, white, white, white, white, white, white, black,
+    black, white, white, white, white, white, white, black,
+    black, white, white, white, white, white, white, black,
+    black, red, red, white, white, gold, gold, black,
+    red, red, black, black, black, black, gold, gold,
+]
+imagine_de_sfarsit_bucata_2_etapa_5 = [
+    blue, blue, white, white, white, white, green, green,
+    blue, white, white, white, white, white, white, green,
+    white, white, white, white, white, white, white, white,
+    white, white, white, white, white, white, white, white,
+    white, white, white, white, white, white, white, white,
+    white, white, white, white, white, white, white, white,
+    red, white, white, white, white, white, white, gold,
+    red, red, white, white, white, white, gold, gold,
+]
+imagine_de_sfarsit_bucata_2_etapa_6 = [
+    white, white, white, white, white, white, white, white,
+    white, white, white, white, white, white, white, white,
+    white, white, white, white, white, white, white, white,
+    white, white, white, white, white, white, white, white,
+    white, white, white, white, white, white, white, white,
+    white, white, white, white, white, white, white, white,
+    white, white, white, white, white, white, white, white,
+    white, white, white, white, white, white, white, white,
 ]
 def afisare_imagine_de_sfarsit_de_nivel_2():
-
-"""
+    sense.set_pixels(imagine_de_sfarsit_bucata_2_etapa_1)
+    sleep(0.5)
+    sense.set_pixels(imagine_de_sfarsit_bucata_2_etapa_2)
+    sleep(0.5)
+    sense.set_pixels(imagine_de_sfarsit_bucata_2_etapa_3)
+    sleep(0.5)
+    sense.set_pixels(imagine_de_sfarsit_bucata_2_etapa_4)
+    sleep(0.5)
+    sense.set_pixels(imagine_de_sfarsit_bucata_2_etapa_5)
+    sleep(0.5)
+    sense.set_pixels(imagine_de_sfarsit_bucata_2_etapa_6)
+    sleep(0.5)
+    sense.clear()
+    
 
 #IMAGINE PENTRU NIVEL INACCESIBIL (IMAGE FOR AN INACCESIBLE LEVEL)
 def locked():
@@ -83,6 +156,15 @@ def unlocked():
     sense.set_pixels(unlock_image)
     sleep(1)
     
+#FUNCTIE PENTRU STORYLINE (FUNCTION FOR THE STORYLINE)
+def Storyline():
+    text = "Spymania is a SenseHat-centred game where Vector the soldier needs to get all the papers in order to find the grand boss who stole a very big and expensive treasure."
+    storyline = text.split(' ')
+    for word in storyline:
+       sense.show_message(word, text_colour = saddlebrown, scroll_speed = 0.04)
+    print(text)
+    afisare_imagine_de_sfarsit_de_nivel_1()
+    afisare_imagine_de_sfarsit_de_nivel_2()
     
 #FUNCTIE DE MAIN MENU (MAIN MENU FUNCTION):
 def MAIN_MENU():
@@ -90,21 +172,12 @@ def MAIN_MENU():
     sense.show_message("to", text_colour = gold, scroll_speed = 0.05)
     sense.show_message("Spymania!", text_colour = green, scroll_speed = 0.05)
     sleep(0.5)
-    sense.show_message("Press the joystick in the middle to continue to the level selection zone!", text_colour = white, scroll_speed = 0.05)
+    sense.show_message("Press the joystick in the middle to continue to the level selection zone!", text_colour = darker_blue, scroll_speed = 0.05)
     event1 = sense.stick.wait_for_event(emptybuffer = True)
-    """
-    ok = False
-    while ok == False:
-        if event1.action == "pressed" and event1.direction == "middle":
-            ok = True
-            break
-        sense.show_message("Press the joystick in the middle to continue!", text_colour = white, scroll_speed = 0.06)
-        sleep (0.5)
-    """
     #IN CAZUL UNEI DORINTE NEPREVAZUTE DE A IESI DIN JOC (IN CASE OF A SUDDEN WISH FOR EXITING THE GAME)
     if event1.action == "pressed" and event1.direction != "middle":
-        sense.show_message("Are you sure you want to exit the game? If yes, press the joystick upwards!", text_colour = red, scroll_speed = 0.04)
-        sense.show_message("If you want to resume, press the joystick downwards!", text_colour = green, scroll_speed = 0.04)
+        sense.show_message("exit? -> press upwards!", text_colour = red, scroll_speed = 0.04)
+        sense.show_message("resume? -> press downwards!", text_colour = green, scroll_speed = 0.04)
         event10 = sense.stick.wait_for_event(emptybuffer = True)
         if event10.action == "pressed" and event10.direction == "up":
             ok1 = True
@@ -115,21 +188,20 @@ def MAIN_MENU():
             sense.show_message("Spymania!", text_colour = green, scroll_speed = 0.05)
             return
         elif event10.action == "pressed" and event10.direction != "up":
-            sense.show_message("Press the joystick in the middle to continue to the level selection zone!", text_colour = white, scroll_speed = 0.05)
+            sense.show_message("Press the joystick in the middle to continue to the level selection zone!", text_colour = darker_blue, scroll_speed = 0.05)
             event1 = sense.stick.wait_for_event(emptybuffer = True)
     #ZONA DE SELECTARE A NIVELULUI DE JOC (THE LEVEL SELECTION ZONE)
     elif event1.action == "pressed" and event1.direction == "middle":
-        sense.show_message("Choose the level you want to play!", text_colour = cyan, scroll_speed = 0.05)
+        sense.show_message("Choose the level you want to play!", text_colour = coral, scroll_speed = 0.05)
         ok1 = False
         position = 0;
         while ok1 == False:
-            #print(position)
             sense.show_message(levels[position], text_colour = gold, scroll_speed = 0.05)
             event2 = sense.stick.wait_for_event(emptybuffer = True)
             #IN CAZUL UNEI DORINTE NEPREVAZUTE DE A IESI DIN JOC (IN CASE OF A SUDDEN WISH FOR EXITING THE GAME)
             if event2.action == "pressed" and event2.direction == "up":
-                sense.show_message("Are you sure you want to exit the game? If yes, press the joystick upwards!", text_colour = red, scroll_speed = 0.04)
-                sense.show_message("If you want to resume, press the joystick downwards!", text_colour = green, scroll_speed = 0.04)
+                sense.show_message("exit? -> press upwards!", text_colour = red, scroll_speed = 0.04)
+                sense.show_message("resume? -> press downwards!", text_colour = green, scroll_speed = 0.04)
                 event9 = sense.stick.wait_for_event(emptybuffer = True)
                 if event9.action == "pressed" and event9.direction == "up":
                     ok1 = True
@@ -145,28 +217,25 @@ def MAIN_MENU():
             if event2.action == "pressed" and event2.direction == "left" and position == 0:
                 position = 0
             elif event2.action == "pressed" and event2.direction == "right" and position == 5:
-                position = 4
+                position = 5
             elif event2.action == "pressed" and event2.direction == "right" and position < 5:
                 position += 1
             elif event2.action == "pressed" and event2.direction == "left" and position > 0:
                 position -= 1
             elif (position == 0 and event2.action == "pressed" and event2.direction == "middle"):
-                #sense.show_message("Storyline", text_colour = gold, scroll_speed = 0.06)
-                sense.show_message("Want to see the Storyline? -> press the joystick in the middle", text_colour = green, scroll_speed = 0.04)                
-                sense.show_message("exit? -> press the joystick in any other way", text_colour = red, scroll_speed = 0.04)
+                sense.show_message("see storyline? -> press middle", text_colour = green, scroll_speed = 0.04)                
+                sense.show_message("exit? -> press any other way", text_colour = red, scroll_speed = 0.04)
                 event3 = sense.stick.wait_for_event(emptybuffer = True)
                 if event3.action == "pressed" and event3.direction == "middle":    
                     if is_playable[position] == 1: 
-                        #Storyline()
-                        unlocked()
+                        Storyline()
                     else:
                         locked()
                 elif event3.action == "pressed" and event3.direction != "middle":
                     continue
             elif (position == 1 and event2.action == "pressed" and event2.direction == "middle"):
-                #sense.show_message("Level 1", text_colour = gold, scroll_speed = 0.06)
-                sense.show_message("Want to play level 1? -> press the joystick in the middle", text_colour = green, scroll_speed = 0.04)                
-                sense.show_message("exit? -> press the joystick in any other way", text_colour = red, scroll_speed = 0.04)
+                sense.show_message("see storyline? -> press middle", text_colour = green, scroll_speed = 0.04)                
+                sense.show_message("exit? -> press any other way", text_colour = red, scroll_speed = 0.04)
                 event4 = sense.stick.wait_for_event(emptybuffer = True)
                 if event4.action == "pressed" and event4.direction == "middle":    
                     if is_playable[position] == 1:
@@ -177,9 +246,8 @@ def MAIN_MENU():
                 elif event4.action == "pressed" and event4.direction != "middle":
                     continue
             elif (position == 2 and event2.action == "pressed" and event2.direction == "middle"):
-                #sense.show_message("Level 2", text_colour = gold, scroll_speed = 0.06)
-                sense.show_message("Want to play level 2? -> press the joystick in the middle", text_colour = green, scroll_speed = 0.04)                
-                sense.show_message("exit? -> press the joystick in any other way", text_colour = red, scroll_speed = 0.04)
+                sense.show_message("see storyline? -> press middle", text_colour = green, scroll_speed = 0.04)                
+                sense.show_message("exit? -> press any other way", text_colour = red, scroll_speed = 0.04)
                 event5 = sense.stick.wait_for_event(emptybuffer = True)
                 if (event5.action == "pressed" and event5.direction == "middle"):    
                     if is_playable[position] == 1: 
@@ -190,9 +258,8 @@ def MAIN_MENU():
                 elif event5.action == "pressed" and event5.direction != "middle":
                     continue
             elif (position == 3 and event2.action == "pressed" and event2.direction == "middle"):
-                #sense.show_message("Level 3", text_colour = gold, scroll_speed = 0.06)
-                sense.show_message("Want to play level 3? -> press the joystick in the middle", text_colour = green, scroll_speed = 0.04)                
-                sense.show_message("exit? -> press the joystick in any other way", text_colour = red, scroll_speed = 0.04)
+                sense.show_message("see storyline? -> press middle", text_colour = green, scroll_speed = 0.04)                
+                sense.show_message("exit? -> press any other way", text_colour = red, scroll_speed = 0.04)
                 event6 = sense.stick.wait_for_event(emptybuffer = True)
                 if (event6.action == "pressed" and event6.direction == "middle"):    
                     if is_playable[position] == 1: 
@@ -203,9 +270,8 @@ def MAIN_MENU():
                 elif event6.action == "pressed" and event6.direction != "middle":
                     continue
             elif (position == 4 and event2.action == "pressed" and event2.direction == "middle"):
-                #sense.show_message("Level 4", text_colour = gold, scroll_speed = 0.06)
-                sense.show_message("Want to play level 4? -> press the joystick in the middle", text_colour = green, scroll_speed = 0.04)                
-                sense.show_message("exit? -> press the joystick in any other way", text_colour = red, scroll_speed = 0.04)
+                sense.show_message("see storyline? -> press middle", text_colour = green, scroll_speed = 0.04)                
+                sense.show_message("exit? -> press any other way", text_colour = red, scroll_speed = 0.04)
                 event7 = sense.stick.wait_for_event(emptybuffer = True)
                 if (event7.action == "pressed" and event7.direction == "middle"):    
                     if is_playable[position] == 1: 
@@ -216,9 +282,8 @@ def MAIN_MENU():
                 elif event7.action == "pressed" and event7.direction != "middle":
                     continue
             elif (position == 5 and event2.action == "pressed" and event2.direction == "middle"):
-                #sense.show_message("Level 5 -> Boss Fight", text_colour = gold, scroll_speed = 0.06)
-                sense.show_message("Want to play level 5 (Boss Fight)? -> press the joystick in the middle", text_colour = green, scroll_speed = 0.04)                
-                sense.show_message("exit? -> press the joystick in any other way", text_colour = red, scroll_speed = 0.04)
+                sense.show_message("see storyline? -> press middle", text_colour = green, scroll_speed = 0.04)                
+                sense.show_message("exit? -> press any other way", text_colour = red, scroll_speed = 0.04)
                 event8 = sense.stick.wait_for_event(emptybuffer = True)
                 if (event8.action == "pressed" and event8.direction == "middle"):    
                     if is_playable[position] == 1: 
@@ -232,6 +297,5 @@ def MAIN_MENU():
     sense.clear()
 
 #Aici apelam toate functiile pe care le-am creat astfel incat jocul sa poata rula (Here we call all the functions we need in order for the game to load)
-
-#afisare_imagine_de_sfarsit_de_nivel_1()
+    
 MAIN_MENU()
