a37edf3fd8606bb6ec352df3ee9c8af63495809d
David Ghergut <davidghergut@gmail.com>
2021-02-09 09:31:59 +0200

    Spymania

--- Game.py
+++ Game.py
@@ -296,6 +296,16 @@ entering_image_3 = [
     black, black, black, coral, coral, black, black, black,
 ]
 
+reset = [
+    black, black, black, saddlebrown, saddlebrown, black, black, black,
+    black, black, black, black, black, black, black, black,
+    grey, grey, black, black, black, black, purple, purple,
+    black, black, black, black, black, black, black, black,
+    gold, orange, black, black, black, black, green, green,
+    black, black, black, black, black, black, black, black,
+    red, red, black, black, black, black, cyan, cyan,
+    black, black, black, coral, coral, black, black, black,
+]
 def ENTERING_MAIN_MENU():
     sense.set_pixels(entering_image_1)
     sleep(0.65)
@@ -324,9 +334,10 @@ def MAIN_MENU():
     sense.show_message("to", text_colour = gold, scroll_speed = 0.05)
     sense.show_message("Spymania!", text_colour = green, scroll_speed = 0.05)
     sleep(0.5)
-    sense.show_message("Press the joystick in the middle to continue!", text_colour = darker_blue, scroll_speed = 0.05)
+    sense.show_message("Press joystick to continue!", text_colour = darker_blue, scroll_speed = 0.05)
     event1 = sense.stick.wait_for_event(emptybuffer = True)
     #IN CAZUL UNEI DORINTE NEPREVAZUTE DE A IESI DIN JOC (IN CASE OF A SUDDEN WISH FOR EXITING THE GAME)
+    """
     if event1.action == "pressed" and event1.direction != "middle":
         sense.show_message("exit? -> up!", text_colour = red, scroll_speed = 0.04)
         sense.show_message("resume? -> down!", text_colour = green, scroll_speed = 0.04)
@@ -342,8 +353,9 @@ def MAIN_MENU():
         elif event10.action == "pressed" and event10.direction != "up":
             sense.show_message("Press the joystick in the middle to continue!", text_colour = darker_blue, scroll_speed = 0.05)
             event1 = sense.stick.wait_for_event(emptybuffer = True)
+    """
     #ZONA DE SELECTARE A NIVELULUI DE JOC (THE LEVEL SELECTION ZONE)
-    elif event1.action == "pressed" and event1.direction == "middle":
+    if event1.action == "pressed":
         sense.show_message("Choose the level you want to play!", text_colour = coral, scroll_speed = 0.05)
         ok1 = False
         pixel1_x = 2
@@ -439,6 +451,10 @@ def MAIN_MENU():
                     pixel2_y = 4
             if event2.action == "pressed" and event2.direction != "middle":
                 pixels = sense.get_pixels()
+                if pixel1_y == 3 and pixel1_x == 1 and pixel2_y == 4 and pixel2_x == 1:
+                    sense.set_pixels(entering_image_2)
+                else:
+                    sense.set_pixels(reset)
                 sense.set_pixel(pixel1_y, pixel1_x, black)
                 sense.set_pixel(pixel2_y, pixel2_x, black)
                 if pixels[3] == black:
@@ -487,106 +503,105 @@ def MAIN_MENU():
                     pixel2_y = pixel2_y + 1
                 sense.set_pixel(pixel1_y, pixel1_x, blue)
                 sense.set_pixel(pixel2_y, pixel2_x, blue)
-            """
-            #IN CAZUL UNEI DORINTE NEPREVAZUTE DE A IESI DIN JOC (IN CASE OF A SUDDEN WISH FOR EXITING THE GAME)
-            if event2.action == "pressed" and event2.direction == "up":
-                sense.show_message("exit? -> up!", text_colour = red, scroll_speed = 0.04)
-                sense.show_message("resume? -> down!", text_colour = green, scroll_speed = 0.04)
-                event9 = sense.stick.wait_for_event(emptybuffer = True)
-                if event9.action == "pressed" and event9.direction == "up":
-                    ok1 = True
-                    sense.show_message("Thank", text_colour = blue, scroll_speed = 0.05)
-                    sense.show_message("you", text_colour = gold, scroll_speed = 0.05)
-                    sense.show_message("for", text_colour = red, scroll_speed = 0.05)
-                    sense.show_message("playing", text_colour = purple, scroll_speed = 0.05)
-                    sense.show_message("Spymania!", text_colour = green, scroll_speed = 0.05)
-                    break
-                elif event9.action == "pressed" and event9.direction == "down":
-                    sense.show_message("Choose the level you want to play!", text_colour = cyan, scroll_speed = 0.05)
-                    event2 = sense.stick.wait_for_event(emptybuffer = True)
-            if event2.action == "pressed" and event2.direction == "left" and position == 0:
-                position = 0
-            elif event2.action == "pressed" and event2.direction == "right" and position == 5:
-                position = 5
-            elif event2.action == "pressed" and event2.direction == "right" and position < 5:
-                position += 1
-            elif event2.action == "pressed" and event2.direction == "left" and position > 0:
-                position -= 1
-            elif (position == 0 and event2.action == "pressed" and event2.direction == "middle"):
-                sense.show_message("see storyline? -> press middle", text_colour = green, scroll_speed = 0.04)                
-                sense.show_message("exit? -> press any other way", text_colour = red, scroll_speed = 0.04)
-                event3 = sense.stick.wait_for_event(emptybuffer = True)
-                if event3.action == "pressed" and event3.direction == "middle":    
-                    if is_playable[position] == 1: 
-                        Storyline()
-                    else:
-                        locked()
-                elif event3.action == "pressed" and event3.direction != "middle":
-                    continue
-            elif (position == 1 and event2.action == "pressed" and event2.direction == "middle"):
-                sense.show_message("see storyline? -> press middle", text_colour = green, scroll_speed = 0.04)                
-                sense.show_message("exit? -> press any other way", text_colour = red, scroll_speed = 0.04)
-                event4 = sense.stick.wait_for_event(emptybuffer = True)
-                if event4.action == "pressed" and event4.direction == "middle":    
-                    if is_playable[position] == 1:
-                        #Level_1()
-                        unlocked()
-                    else:
-                        locked()
-                elif event4.action == "pressed" and event4.direction != "middle":
-                    continue
-            elif (position == 2 and event2.action == "pressed" and event2.direction == "middle"):
-                sense.show_message("see storyline? -> press middle", text_colour = green, scroll_speed = 0.04)                
-                sense.show_message("exit? -> press any other way", text_colour = red, scroll_speed = 0.04)
-                event5 = sense.stick.wait_for_event(emptybuffer = True)
-                if (event5.action == "pressed" and event5.direction == "middle"):    
-                    if is_playable[position] == 1: 
-                        #Level_2()
-                        unlocked()
-                    else:
-                        locked()
-                elif event5.action == "pressed" and event5.direction != "middle":
-                    continue
-            elif (position == 3 and event2.action == "pressed" and event2.direction == "middle"):
-                sense.show_message("see storyline? -> press middle", text_colour = green, scroll_speed = 0.04)                
-                sense.show_message("exit? -> press any other way", text_colour = red, scroll_speed = 0.04)
-                event6 = sense.stick.wait_for_event(emptybuffer = True)
-                if (event6.action == "pressed" and event6.direction == "middle"):    
-                    if is_playable[position] == 1: 
-                        #Level_3()
-                        unlocked()
-                    else:
-                        locked()
-                elif event6.action == "pressed" and event6.direction != "middle":
-                    continue
-            elif (position == 4 and event2.action == "pressed" and event2.direction == "middle"):
-                sense.show_message("see storyline? -> press middle", text_colour = green, scroll_speed = 0.04)                
-                sense.show_message("exit? -> press any other way", text_colour = red, scroll_speed = 0.04)
-                event7 = sense.stick.wait_for_event(emptybuffer = True)
-                if (event7.action == "pressed" and event7.direction == "middle"):    
-                    if is_playable[position] == 1: 
-                        #Level_4()
-                        unlocked()
-                    else:
-                        locked()
-                elif event7.action == "pressed" and event7.direction != "middle":
-                    continue
-            elif (position == 5 and event2.action == "pressed" and event2.direction == "middle"):
-                sense.show_message("see storyline? -> press middle", text_colour = green, scroll_speed = 0.04)                
-                sense.show_message("exit? -> press any other way", text_colour = red, scroll_speed = 0.04)
-                event8 = sense.stick.wait_for_event(emptybuffer = True)
-                if (event8.action == "pressed" and event8.direction == "middle"):    
-                    if is_playable[position] == 1: 
-                        #Level_5()
-                        unlocked()
-                    else:
-                        locked()
-                elif event8.action == "pressed" and event8.direction != "middle":
-                    continue
-            """
-            
     #La finalul programului, dezactivam toate led-urile aprinse de pe SenseHat (At the end of the program, we clear the SenseHat)
     sense.clear()
+    """
+    #IN CAZUL UNEI DORINTE NEPREVAZUTE DE A IESI DIN JOC (IN CASE OF A SUDDEN WISH FOR EXITING THE GAME)
+    if event2.action == "pressed" and event2.direction == "up":
+        sense.show_message("exit? -> up!", text_colour = red, scroll_speed = 0.04)
+        sense.show_message("resume? -> down!", text_colour = green, scroll_speed = 0.04)
+        event9 = sense.stick.wait_for_event(emptybuffer = True)
+        if event9.action == "pressed" and event9.direction == "up":
+            ok1 = True
+            sense.show_message("Thank", text_colour = blue, scroll_speed = 0.05)
+            sense.show_message("you", text_colour = gold, scroll_speed = 0.05)
+            sense.show_message("for", text_colour = red, scroll_speed = 0.05)
+            sense.show_message("playing", text_colour = purple, scroll_speed = 0.05)
+            sense.show_message("Spymania!", text_colour = green, scroll_speed = 0.05)
+            break
+        elif event9.action == "pressed" and event9.direction == "down":
+            sense.show_message("Choose the level you want to play!", text_colour = cyan, scroll_speed = 0.05)
+            event2 = sense.stick.wait_for_event(emptybuffer = True)
+    if event2.action == "pressed" and event2.direction == "left" and position == 0:
+        position = 0
+    elif event2.action == "pressed" and event2.direction == "right" and position == 5:
+        position = 5
+    elif event2.action == "pressed" and event2.direction == "right" and position < 5:
+        position += 1
+    elif event2.action == "pressed" and event2.direction == "left" and position > 0:
+        position -= 1
+    elif (position == 0 and event2.action == "pressed" and event2.direction == "middle"):
+        sense.show_message("see storyline? -> press middle", text_colour = green, scroll_speed = 0.04)                
+        sense.show_message("exit? -> press any other way", text_colour = red, scroll_speed = 0.04)
+        event3 = sense.stick.wait_for_event(emptybuffer = True)
+        if event3.action == "pressed" and event3.direction == "middle":    
+            if is_playable[position] == 1: 
+                Storyline()
+            else:
+                locked()
+        elif event3.action == "pressed" and event3.direction != "middle":
+            continue
+    elif (position == 1 and event2.action == "pressed" and event2.direction == "middle"):
+        sense.show_message("see storyline? -> press middle", text_colour = green, scroll_speed = 0.04)                
+        sense.show_message("exit? -> press any other way", text_colour = red, scroll_speed = 0.04)
+        event4 = sense.stick.wait_for_event(emptybuffer = True)
+        if event4.action == "pressed" and event4.direction == "middle":    
+            if is_playable[position] == 1:
+                #Level_1()
+                unlocked()
+            else:
+                locked()
+        elif event4.action == "pressed" and event4.direction != "middle":
+            continue
+    elif (position == 2 and event2.action == "pressed" and event2.direction == "middle"):
+        sense.show_message("see storyline? -> press middle", text_colour = green, scroll_speed = 0.04)                
+        sense.show_message("exit? -> press any other way", text_colour = red, scroll_speed = 0.04)
+        event5 = sense.stick.wait_for_event(emptybuffer = True)
+        if (event5.action == "pressed" and event5.direction == "middle"):    
+            if is_playable[position] == 1: 
+                #Level_2()
+                unlocked()
+            else:
+                locked()
+        elif event5.action == "pressed" and event5.direction != "middle":
+            continue
+    elif (position == 3 and event2.action == "pressed" and event2.direction == "middle"):
+        sense.show_message("see storyline? -> press middle", text_colour = green, scroll_speed = 0.04)                
+        sense.show_message("exit? -> press any other way", text_colour = red, scroll_speed = 0.04)
+        event6 = sense.stick.wait_for_event(emptybuffer = True)
+        if (event6.action == "pressed" and event6.direction == "middle"):    
+            if is_playable[position] == 1: 
+                #Level_3()
+                unlocked()
+            else:
+                locked()
+        elif event6.action == "pressed" and event6.direction != "middle":
+            continue
+    elif (position == 4 and event2.action == "pressed" and event2.direction == "middle"):
+        sense.show_message("see storyline? -> press middle", text_colour = green, scroll_speed = 0.04)                
+        sense.show_message("exit? -> press any other way", text_colour = red, scroll_speed = 0.04)
+        event7 = sense.stick.wait_for_event(emptybuffer = True)
+        if (event7.action == "pressed" and event7.direction == "middle"):    
+            if is_playable[position] == 1: 
+                #Level_4()
+                unlocked()
+            else:
+                locked()
+        elif event7.action == "pressed" and event7.direction != "middle":
+            continue
+    elif (position == 5 and event2.action == "pressed" and event2.direction == "middle"):
+        sense.show_message("see storyline? -> press middle", text_colour = green, scroll_speed = 0.04)                
+        sense.show_message("exit? -> press any other way", text_colour = red, scroll_speed = 0.04)
+        event8 = sense.stick.wait_for_event(emptybuffer = True)
+        if (event8.action == "pressed" and event8.direction == "middle"):    
+            if is_playable[position] == 1: 
+                #Level_5()
+                unlocked()
+            else:
+                locked()
+        elif event8.action == "pressed" and event8.direction != "middle":
+            continue
+    """
 
 #Aici apelam toate functiile pe care le-am creat astfel incat jocul sa poata rula (Here we call all the functions we need in order for the game to load)
     
